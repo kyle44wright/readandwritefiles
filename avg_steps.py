2ae8e9ab8a7dadee3aed2aa2f3a682aadee7e849
Kyle <kw@test.com>
2022-09-08 12:26:26 -0500

    employee bonus done and start avg steps

---
+++ avg_steps.py
@@ -0,0 +1,17 @@
+import csv
+
+infile = open('steps.csv','r')
+outfile = open("avg_steps.csv.csv",'w')
+
+csvfile = csv.reader(infile, delimiter=',')
+
+
+outfile.write("Month, Steps\n")
+
+for record in csvfile:
+    
+    if x 
+
+    outfile.write()
+
+outfile.close()
